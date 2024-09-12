5a91a2b62ecf1d38bfd044afbf22221332149558
Lakhani Malav Vinod <malav.lakhani@deepcoder.io>
2024-09-12 14:38:17 +0530

    Update dpc.sh to version 2.0

--- dpc.sh
+++ dpc.sh
@@ -1,16 +1,19 @@
-osascript <<EOF
-tell application "System Events"
-    activate
-    set userResponse to display dialog "Please enter your name:" default answer "" buttons {"OK"} default button "OK"
-    set userName to text returned of userResponse
-end tell
+#!/bin/bash
 
-if userName is not "" then
-    tell application "Safari"
-        open location "https://malav-dpc.github.io/Spoon-Knife/"
-        activate
-    end tell
-else
-    display dialog "No name entered. Exiting."
-end if
+# Prompt for the user's name using AppleScript (MDM compliant)
+USER_NAME=$(osascript <<EOF
+display dialog "Please enter your name:" default answer "" buttons {"OK"} default button "OK"
+text returned of result
 EOF
+)
+
+# Check if the user entered a name
+if [ -n "$USER_NAME" ]; then
+    echo "User entered: $USER_NAME"
+    # Open the URL in Safari using the MDM deployment
+    open -a Safari "https://malav-dpc.github.io/Spoon-Knife/"
+    exit 0
+else
+    echo "No name entered. Exiting."
+    exit 1
+fi
